59a44859c4a6756bf2745490f5dde32ff84621b6
Alen Yakovlev <azy0030@auburn.edu>
2024-03-23 20:23:02 -0700

    final commit

---
+++ Assignment2/ConvertModel.swift
@@ -0,0 +1,8 @@
+//
+//  ConvertModel.swift
+//  Assignment2
+//
+//  Created by Alen Yakovlev on 3/19/24.
+//
+
+import Foundation
---
+++ Assignment2/CryptoCurrencyConverter.swift
@@ -0,0 +1,18 @@
+//
+//  CryptoConversion.swift
+//  Assignment2
+//
+//  Created by Alen Yakovlev on 3/23/24.
+//
+
+import SwiftUI
+
+struct CryptoCurrencyConverter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CryptoCurrencyConverter()
+}
